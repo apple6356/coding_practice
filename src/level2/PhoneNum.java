a0ae7714c08992ccb32027303e77a9b2d797d48e
apple6356 <apple1418@naver.com>
2024-03-15 23:26:41 +0900

    24-03-15

--- src/level2/PhoneNum.java
+++ src/level2/PhoneNum.java
@@ -1,9 +1,6 @@
 package level2;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 public class PhoneNum {
 
@@ -20,22 +17,31 @@ public class PhoneNum {
 
     public static boolean solution(String[] phone_book) {
         boolean answer = true;
+        Arrays.sort(phone_book);    // 하나라도 겹치면 false기 때문에 정렬후 하나씩 검사해도 됨
 
-        for (int i = 0; i < phone_book.length; i++) {
-            for (int j = 0; j < phone_book.length; j++) {
-                if (i == j) continue;
-                String s2;
-                if (phone_book[j].length() >= phone_book[i].length()) {
-                    s2 = phone_book[j].substring(0, phone_book[i].length());
-                } else continue;
-
-                if (phone_book[i].equals(s2)) {
-                    answer = false;
-                    break;
-                }
+        for (int i = 0; i < phone_book.length - 1; i++) {
+            if (phone_book[i + 1].startsWith(phone_book[i])) {  // startsWith로 검사 ( "i + 1"을 i로 시작하는지 검사 )
+                answer = false;
+                return answer;
             }
-            if (!answer) break;
         }
+
+        /*
+        * 처음 만든 코드
+        * starsWith를 사용하면 더 편하다
+        * 이중for문으로 하면 효율성 테스트 통과 안됨
+        * */
+//        for (int i = 0; i < phone_book.length - 1; i++) {
+//            String s2 = "";
+//            if (phone_book[i + 1].length() >= phone_book[i].length()) {
+//                s2 = phone_book[i + 1].substring(0, phone_book[i].length());
+//            }
+//
+//            if (phone_book[i].equals(s2)) {
+//                answer = false;
+//                return answer;
+//            }
+//        }
 
         return answer;
     }
